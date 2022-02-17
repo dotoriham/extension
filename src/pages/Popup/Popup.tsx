3067d4553b2058e186c1f8467d40cf62e99580ce
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-02-17 11:30:55 +0900

    feat: 폴더 데이터 불러오기 성공

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,8 +1,8 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-import { getStorageTokens } from './getStorageTokens';
 
 export interface TodoType {
   id: number;
@@ -11,37 +11,22 @@ export interface TodoType {
 }
 
 function getStorage() {
-  const accessToken = localStorage.getItem('accessToken');
-  return accessToken;
+  const userToken = localStorage.getItem('userToken') || '';
+  return JSON.parse(userToken);
 }
 
-function executeScript(msg: any, callback: any) {
+function executeScript(callback: any) {
   if (chrome?.tabs?.query) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
       const tabId: any = tab[0].id;
       const exec = chrome.scripting.executeScript;
-      console.log('exec', exec);
-      console.log('tabId', tabId);
-
       exec(
         { target: { tabId: tabId }, function: getStorage },
         function (response) {
           console.log(response);
-          callback && callback(response[0].result);
+          callback && callback(response[0].result); // 있을 경우에만 리턴하도록 설정 없으면 아무것도 리턴 안하니간 local에 저장 안될테니 걱정 ㄴㄴ;
         },
       );
-
-      //   exec(tabId, { code: `var msg = ${JSON.stringify(msg)}` }, function () {
-      //     if (chrome.runtime.lastError) {
-      //       console.log(chrome.runtime.lastError.message);
-      //       callback && callback(undefined);
-      //       return;
-      //     }
-
-      //     exec(tabId, { file: 'inject.js' }, function (response) {
-      //       callback && callback(response[0]);
-      //     });
-      //   });
     });
   }
 }
@@ -60,30 +45,30 @@ const Popup = () => {
 
   useEffect(() => {
     const fetch = () => {
-      const { refreshToken: userRefreshToken, accessToken: userAccessToken } =
-        getStorageTokens();
-      executeScript({ what: 'get', type: 'L' }, async function (response: any) {
+      executeScript(async function (response: any) {
         console.log('response', response);
-        const accessToken = response.accessToken;
-        const refreshToken = response.refreshToken;
-        let checkIsLogin = accessToken ? !!accessToken : !!userAccessToken;
-        localStorage.setItem(
-          'refreshToken',
-          refreshToken ? refreshToken : userRefreshToken,
-        );
-        localStorage.setItem(
-          'accessToken',
-          accessToken ? accessToken : userAccessToken,
-        );
-        console.log(
-          accessToken,
-          refreshToken,
-          userAccessToken,
-          userRefreshToken,
-        );
+        localStorage.setItem('userToken', JSON.stringify(response));
       });
     };
+
+    const getFolderData = async () => {
+      const userToken = localStorage.getItem('userToken');
+      if (userToken) {
+        const { accessToken, refreshToken } = JSON.parse(userToken);
+        const res = await axios.get(
+          `https://dotoriham.duckdns.org/api/v1/page/main?page=0&size=9&sort=saveTime,desc&remind=false`,
+          {
+            headers: {
+              accessToken: `Bearer ${accessToken}`,
+              refreshToken: `Bearer ${refreshToken}`,
+            },
+          },
+        );
+        console.log('찐', res);
+      }
+    };
     fetch();
+    getFolderData();
   }, []);
 
   const onInsert = (text: string) => {
