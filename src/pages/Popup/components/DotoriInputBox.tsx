7146014e2c267f814b898d06982e49ade023fbd0
이도현 <ksmfou98@naver.com>
2022-02-19 01:19:18 +0900

    feat: Dotori Title Input 스타일링

--- src/pages/Popup/components/DotoriInputBox.tsx
+++ src/pages/Popup/components/DotoriInputBox.tsx
@@ -2,12 +2,24 @@ import React, { ReactElement } from 'react';
 import styled from 'styled-components';
 
 function DotoriInputBox(): ReactElement {
-  return <DotoriInputBoxStyled>DotoriInput</DotoriInputBoxStyled>;
+  return (
+    <DotoriInputBoxStyled>
+      <TitleInput type="text" placeholder="제목을 입력해주세요" />
+    </DotoriInputBoxStyled>
+  );
 }
 
 const DotoriInputBoxStyled = styled.div`
   margin: 24px;
   width: 150px;
+`;
+
+const TitleInput = styled.input`
+  border: 1px solid #ddd;
+  width: 100%;
+  border-radius: 4px;
+  font-size: 12px;
+  padding: 5px 12px 6px 8px;
 `;
 
 export default DotoriInputBox;
