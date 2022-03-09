1dc4265d5b41df1703894c3566d467ef6442abc0
이도현 <ksmfou98@naver.com>
2022-03-09 10:22:01 +0900

    feat: 익스텐션 로그인 session 관리

--- src/pages/Popup/lib/api/client.ts
+++ src/pages/Popup/lib/api/client.ts
@@ -27,4 +27,54 @@ client.interceptors.request.use(
   },
 );
 
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+
+    switch (status) {
+      case 401:
+        const originalRequest = config;
+        const tokens = getTokens();
+        if (!tokens) throw new Error('No tokens found');
+        const { accessToken, refreshToken } = tokens;
+        try {
+          const { data } = await axios.get(
+            `${SERVER_URL}/api/v1/user/reIssuanceAccessToken`,
+            {
+              headers: {
+                accessToken: `Bearer ${accessToken}`,
+                refreshToken: `Bearer ${refreshToken}`,
+              },
+            },
+          );
+
+          const newTokens = {
+            accessToken: data.accessToken,
+            refreshToken: data.refreshToken,
+          };
+          localStorage.setItem('userToken', JSON.stringify(newTokens));
+          axios.defaults.headers.common.accessToken = `Bearer ${data.accessToken}`;
+          axios.defaults.headers.common.refreshToken = `Bearer ${data.refreshToken}`;
+          originalRequest.headers.accessToken = `Bearer ${data.accessToken}`;
+          originalRequest.headers.refreshToken = `Bearer ${data.refreshToken}`;
+          // 401로 요청 실패했던 요청 새로운 accessToken으로 재요청
+          return axios(originalRequest);
+        } catch (e) {
+          localStorage.removeItem('userToken');
+          chrome.extension.getViews({ type: 'popup' })[0].location.reload();
+        }
+        break;
+
+      default:
+        throw new Error('Unknown Error');
+    }
+  },
+);
+
 export default client;
