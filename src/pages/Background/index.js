796057237020dd9f7acf9c2554c4f685dda1f814
이도현 <ksmfou98@naver.com>
2022-04-18 17:07:01 +0900

    chore: extension version 4.0.6

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,5 +1,4 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log('asdasd', request.dotoriUsers);
   if (sender.tab.url.includes('dotoriham.com')) {
     chrome.storage.local.set({ DOTORI_USER: request.dotoriUsers });
   }
